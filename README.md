b3e636d28221d25f17fbe1821d8715e0e5a614a7
gox <gox.roche@gmail.com>
2025-04-05 17:17:03 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# mongodb-logback-appender
---
+++ src/main/java/org/gox/logback/appender/mongodb/Main.java
@@ -0,0 +1,7 @@
+package org.gox.logback.appender.mongodb;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
