caa664fd984bde0ad707836a0560342afb652fb3
gox <gox.roche@gmail.com>
2025-04-08 19:38:31 +0200

    Finalize mongodb appender

--- src/main/java/org/gox/logback/appender/MongoDbAppender.java
+++ src/main/java/org/gox/logback/appender/MongoDbAppender.java
@@ -1,22 +1,81 @@
 package org.gox.logback.appender;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.AsyncAppenderBase;
+import ch.qos.logback.core.UnsynchronizedAppenderBase;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.result.InsertOneResult;
+import org.bson.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class MongoDbAppender extends AsyncAppenderBase<ILoggingEvent> {
+import java.time.ZoneId;
+
+public class MongoDbAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
+
+    private static final Logger logger = LoggerFactory.getLogger(MongoDbAppender.class);
+
+    private String connectionString;
+    private String collectionName;
+    private MongoClient mongoClient;
 
     @Override
     public void start() {
-        super.start();
+        try {
+            MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                    .applyConnectionString(new ConnectionString(connectionString))
+                    .build();
+            this.mongoClient = MongoClients.create(mongoClientSettings);
+            super.start();
+            logger.info("MongoDB appender started: {}", isStarted());
+        } catch (Exception e) {
+            logger.error("Error creating MongoDB appender", e);
+        }
     }
 
     @Override
     public void stop() {
+        this.mongoClient.close();
         super.stop();
     }
 
     @Override
-    protected void append(ILoggingEvent eventObject) {
-        super.append(eventObject);
+    protected void append(ILoggingEvent event) {
+        MongoCollection<Document> collection = mongoClient
+                .getDatabase("test")
+                .getCollection(this.collectionName);
+
+        Document document = new Document();
+        document.append("timestamp", event.getInstant().atZone(ZoneId.systemDefault()).toLocalDateTime())
+                .append("level", event.getLevel().toString())
+                .append("logger", event.getLoggerName())
+                .append("message", event.getFormattedMessage())
+                .append("thread", event.getThreadName());
+
+        Thread.startVirtualThread(() -> {
+            InsertOneResult insertOneResult = collection.insertOne(document);
+            if (logger.isDebugEnabled()) {
+                logger.debug("Inserted document : {}, ACK : {}", insertOneResult, insertOneResult.wasAcknowledged());
+            }
+        });
+    }
+
+    public String getConnectionString() {
+        return connectionString;
+    }
+
+    public void setConnectionString(String connectionString) {
+        this.connectionString = connectionString;
+    }
+
+    public String getCollectionName() {
+        return collectionName;
+    }
+
+    public void setCollectionName(String collectionName) {
+        this.collectionName = collectionName;
     }
 }
--- src/test/java/org/gox/logback/appender/MongoDbAppenderTest.java
+++ src/test/java/org/gox/logback/appender/MongoDbAppenderTest.java
@@ -1,5 +1,68 @@
 package org.gox.logback.appender;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.spi.LoggingEvent;
+import com.mongodb.client.*;
+import org.bson.Document;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.MongoDBContainer;
+
+import java.time.Instant;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class MongoDbAppenderTest {
 
+    private static final Logger log = LoggerFactory.getLogger(MongoDbAppenderTest.class);
+    private static MongoDBContainer mongoContainer;
+    private static MongoClient mongoClient;
+    private static String replicaSetUrl;
+
+    @BeforeAll
+    static void setUp() {
+        mongoContainer = new MongoDBContainer("mongo:6.0");
+        mongoContainer.start();
+        replicaSetUrl = mongoContainer.getReplicaSetUrl();
+        mongoClient = MongoClients.create(replicaSetUrl);
+    }
+
+    @AfterAll
+    static void tearDown() {
+        mongoClient.close();
+        mongoContainer.stop();
+    }
+
+    @Test
+    void testMongoDbAppender() throws InterruptedException {
+        MongoDbAppender mongoDbAppender = new MongoDbAppender();
+        mongoDbAppender.setConnectionString(replicaSetUrl);
+        System.out.println(replicaSetUrl);
+        mongoDbAppender.setCollectionName("testLogs");
+        mongoDbAppender.start();
+
+        LoggingEvent loggingEvent = new LoggingEvent();
+        loggingEvent.setInstant(Instant.now());
+        loggingEvent.setThreadName("Test");
+        loggingEvent.setLevel(Level.INFO);
+        loggingEvent.setLoggerName("LOGGER");
+        loggingEvent.setMessage("HELLO THERE");
+
+        mongoDbAppender.append(loggingEvent);
+        Thread.sleep(1000);
+
+        MongoCollection<Document> logs = mongoClient.getDatabase("test").getCollection("testLogs");
+        assertThat(logs.countDocuments()).isEqualTo(1);
+
+        assertThat(Optional.ofNullable(logs.find().first())
+                .map(document -> document.getString("message"))
+                .stream()
+                .findFirst()
+                .orElseThrow()
+        ).isEqualTo("HELLO THERE");
+    }
 }
